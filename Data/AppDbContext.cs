0df73a651035722295eb9994f77d8292fa52b010
kortega6-glitch <kortega6@ucol.mx>
2026-05-27 23:15:32 -0600

    Add files via upload

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ZapateriaApp.Models;
+
+namespace ZapateriaApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Cliente> Clientes => Set<Cliente>();
+
+        public DbSet<Venta> Ventas => Set<Venta>();
+
+        public DbSet<DetalleVenta> DetalleVentas => Set<DetalleVenta>();
+    }
+}
---
+++ Models/Venta.cs
@@ -0,0 +1,19 @@
+﻿namespace ZapateriaApp.Models
+{
+    public class Venta
+    {
+        public int Id { get; set; }
+
+        public int ClienteId { get; set; }
+
+        public Cliente? Cliente { get; set; }
+
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+        public decimal Total { get; set; }
+
+        public string Estado { get; set; } = "Completada";
+
+        public List<DetalleVenta>? Detalles { get; set; }
+    }
+}
